f1f76ae7d2605ac2dd417d93e1bb27a16c6b8e6f
abelRmolina <78396129+abelRmolina@users.noreply.github.com>
2023-01-16 23:59:45 -0300

    actualizacion

--- src/app.js
+++ src/app.js
@@ -1,13 +1,21 @@
 const express = require('express');
 const mainRouter = require('./routes/main');
-
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const acceso = require ('./middlewares/acceso')
 const app = express();
 
+app.use(cookieParser())
+app.use(session({secret: 'secret'}))
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(acceso)
+
 
 app.set('view engine', 'ejs');
 app.set('views', 'src/views');
+
+
 
 app.use('/', mainRouter);
 
